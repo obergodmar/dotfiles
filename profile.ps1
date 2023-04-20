b89c1132f5d769813cedcc5d3330e303f576ead9
Vitaly <obergodmar@gmail.com>
2023-04-21 00:54:18 +0300

    feat: add pwsh profile

---
+++ profile.ps1
@@ -0,0 +1,5 @@
+Import-Module posh-git
+
+function rename_wezterm_title($title) {
+  echo "$([char]27)]1337;SetUserVar=panetitle=$([Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($title)))$([char]7)"
+}
