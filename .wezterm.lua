71040f2798decee4f0f30b59f32be50d4ed247e1
Vitaly <obergodmar@gmail.com>
2023-05-27 03:27:53 +0300

    feat: wezterm optimisation for windows

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,30 +1,34 @@
+local config = {}
+
 local wezterm = require 'wezterm'
 local act = wezterm.action
-local config = {}
 
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-
 -- Configs for Windows only
--- if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
--- end
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  config.default_prog = { 'pwsh.exe', '-WorkingDirectory', '~' }
+  config.font_size = 12
+  config.font = wezterm.font('Iosevka Nerd Font', { weight = 'Bold' })
+  config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+end
 
 -- Configs for OSX only
--- if wezterm.target_triple == 'x86_64-apple-darwin' then
--- end
+if wezterm.target_triple == 'x86_64-apple-darwin' then
+  config.font_size = 11
+  config.term = 'wezterm'
+end
 
 -- Configs for Linux only
 if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
   config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+  config.font_size = 11
+  config.term = 'wezterm'
 end
 
-
 config.color_scheme = 'Dracula'
-config.font = wezterm.font 'Iosevka Nerd Font Mono SemiBold'
-config.font_size = 11
-config.term = 'wezterm'
 
 wezterm.on("format-tab-title", function(tab)
   local pane_title = tab.active_pane.title
