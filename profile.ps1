da4874ba8bd7ae847055a83ab04708b900f6b48a
Vitaly <obergodmar@gmail.com>
2024-06-13 00:12:45 +0300

    feat: add posh git integration

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,7 @@
-oh-my-posh init pwsh --config 'C:/Users/obergodmar/Code/dotfiles/obergodmar.omp.json' | Invoke-Expression
+oh-my-posh init pwsh --config 'C:/Users/oberg/Code/dotfiles/obergodmar.omp.json' | Invoke-Expression
 
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+Import-Module posh-git
 
 function rename_wezterm_title($title) {
   echo "$([char]27)]1337;SetUserVar=panetitle=$([Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($title)))$([char]7)"
